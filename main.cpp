4c61638b5616559b001e1c37e9f814afde8d7347
Anthony Gonzalez <anthonyg9942@gmail.com>
2017-10-31 13:12:53 -0400

    labz

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: agonzalez
+ *
+ * Created on October 31, 2017, 12:55 PM
+ */
+
+#include <iostream> 
+using namespace std; 
+int main(void) {
+int a = 8, b = 0, c = 0; 
+cin >> b;
+try {
+  //Your code here
+c = a / b; }
+ // Your code here
+ cout << c << endl;
+return 0; }
+
